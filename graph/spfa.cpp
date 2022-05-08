01d1b9cc04daaa152197f58216c344a101f3dd68
Yijun Li <Mopriestt@gmail.com>
2022-05-08 16:31:51 +1000

    Spfa doc

--- graph/spfa.cpp
+++ graph/spfa.cpp
@@ -4,71 +4,74 @@
 
 using namespace std;
 
-
 template <typename T>
 
 /**
  * Bellman-Ford's algorithm with queue optimised.
  * AKA Shortest-Path-Fast-Algorithm.
-*/
+ * To use:
+ * Spfa<Type> spfa(node_count);
+ * spfa.add_edge(a1, b1, c1);
+ * spfa.add_edge(a2, b2, c2);
+ * ...
+ * spfa.calculate(start_node_index);
+ * result = spfa.distance
+ */
 class Spfa {
-    public:
-        int n;
-        bool undirected;
-        
-        vector<T> distance;
-        vector<bool> visited;
-        
-        Spfa(int n, bool undirected = false) {
-            this -> n = n;
-            this -> undirected = undirected;
-            distance.resize(n);
-            visited.resize(n);
-            edges.resize(n);
+public:
+    int n;
+    bool undirected;
+
+    vector<T> distance;
+    vector<bool> visited;
+
+    Spfa(int n, bool undirected = false) {
+        this -> n = n;
+        this -> undirected = undirected;
+        distance.resize(n);
+        visited.resize(n);
+        edges.resize(n);
+    }
+
+    void add_edge(int start, int end, T length) {
+        index_sanity_check(start, "add_edge");
+        index_sanity_check(end, "add_edge");
+
+        edges[start].push_back(make_pair(end, length));
+        if (undirected) edges[end].push_back(make_pair(start, length));
+    }
+
+    void calculate(int start) {
+        index_sanity_check(start, "calculate");
+
+        while (!q.empty()) q.pop();
+        for (int i = 0 ; i < n; i ++) visited[i] = false;
+        distance[start] = 0;
+        visited[start] = true;
+        q.push(start);
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (auto next : edges[u]) relax(u, next.first, next.second);
         }
-        
-        void add_edge(int start, int end, T length) {
-            index_sanity_check(start, "add_edge");
-            index_sanity_check(end, "add_edge");
-            
-            edges[start].push_back(make_pair(end, length));
-            if (undirected) edges[end].push_back(make_pair(start, length));
+    }
+private:
+    queue<int> q;
+    vector<vector<pair<int, T>>> edges;
+
+    void relax(int u, int v, T l) {
+        if (!visited[v] || distance[v] > distance[u] + l) {
+            visited[v] = true;
+            distance[v] = distance[u] + l;
+            q.push(v);
         }
-        
-        void calculate(int start) {
-            index_sanity_check(start, "calculate");
-            
-            while (!q.empty()) q.pop();
-            for (int i = 0 ; i < n; i ++) visited[i] = false;
-            distance[start] = 0;
-            visited[start] = true;
-            q.push(start);
-            
-            while (!q.empty()) {
-                int u = q.front();
-                q.pop();
-                
-                for (auto next : edges[u]) {
-                    relax(u, next.first, next.second);
-                }
-            }
-        }
-    private:
-        queue<int> q;
-        vector<vector<pair<int, T>>> edges;
-        
-        void relax(int u, int v, T l) {
-            if (!visited[v] || distance[v] > distance[u] + l) {
-                visited[v] = true;
-                distance[v] = distance[u] + l;
-                q.push(v);
-            }
-        }
-        
-        void index_sanity_check(int index, string where) {
-            if (index >= n) throw std::overflow_error(where + ": index >= n");
-            if (index < 0) throw std::overflow_error(where + ": index < 0");
-        }
+    }
+
+    void index_sanity_check(int index, string where) {
+        if (index >= n) throw std::overflow_error(where + ": index >= n");
+        if (index < 0) throw std::overflow_error(where + ": index < 0");
+    }
 };
 
 void test_spfa_with_floyd(int N) {

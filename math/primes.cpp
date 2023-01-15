f78279789c4c4dfa2ee32e9aeb23879074dce806
Yijun Li <Mopriestt@gmail.com>
2023-01-15 13:19:45 +1100

    Add prime filter and comments

--- math/primes.cpp
+++ math/primes.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+// Returns prime filter for [1, n]
+vector<bool> getPrimeF(int n) {
+    vector<bool> f(n + 1, true);
+    for (long long i = 2; i * i <= n; i ++) {
+        if (!f[i]) continue;
+        for (long long j = i * i; j <= n; j += i)
+            f[j] = false;
+    }
+    f[0] = f[1] = false;
+    return f;
+}
+
+// Returns primes in [1, n]
 vector<long long> getPrimes(long long n) {
     vector<bool> f(n + 1, true);
     vector<long long> ret;
@@ -17,6 +30,7 @@ vector<long long> getPrimes(long long n) {
     return ret;
 }
 
+// Returns primes in [l, r]
 vector<long long> getPrimes(long long l, long long r) {
     if (r < l) {
         cout<<"getPrimes r < l"<<endl;
@@ -41,6 +55,13 @@ vector<long long> getPrimes(long long l, long long r) {
     return ret;
 }
 
+void test_prime_f(long long n) {
+    auto p = getPrimeF(n);
+    for (long long i : p) cout<<i<<' ';
+    cout<<endl;
+
+}
+
 void test_prime_n(long long n) {
     auto p = getPrimes(n);
     for (long long i : p) cout<<i<<' ';
@@ -54,6 +75,9 @@ void test_prime_lr(long long l, long long r) {
 }
 
 int main() {
+    cout<<"test prime filter 1 to N"<<endl<<endl;
+    test_prime_f(100);
+
     cout<<"test primes 1 to N"<<endl<<endl;
     test_prime_n(1000);
 

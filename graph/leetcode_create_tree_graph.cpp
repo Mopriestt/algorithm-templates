7b3875d8c6b0cec4775f6bfeeae976417b3d2963
Yijun Li <Mopriestt@gmail.com>
2023-10-02 01:02:46 +1100

    Sap skeleton.

--- graph/leetcode_create_tree_graph.cpp
+++ graph/leetcode_create_tree_graph.cpp
@@ -1,26 +1,25 @@
-const int MAXN = 100005; //1e5
+#include <iostream>
+#include <vector>
 
-// !!!! REMEMBER TO RESET v AND es !!!!
+using namespace std;
 
-struct Edge {
-    int t;
-    Edge *p;
-} *v[MAXN], e[MAXN * 2];
-
-int es = -1;
-
-void add_edge(int i, int j) {
-    e[++es].t = j; e[es].p = v[i]; v[i] = e + es;
-}
-
-// Tree calc father.
-int father[MAXN];
-void getFather(int u, int fa) {
-    father[u] = fa;
-    for (auto x = v[u]; x; x = x -> p) if (x -> t != fa) {
-        getFather(x -> t, u);
+class Solution {
+    int n;
+    vector<vector<int>> g;
+    void addedges(vector<vector<int>> &edges) {
+        g.resize(n);
+        for (auto &e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]); // Remove if graph is unidrectional
+        }
     }
-}
+
+    void dfs(int u, int fa) {
+        for (auto v : g[u]) if (v != fa) {
+            dfs(v, u);
+        }
+    }
+};
 
 int main() {
     return 0;
